8aabe1c3c3fa765fd4d239c3576123cfefb72ef0
cw-exception <20021064@coway.com>
2022-02-04 17:55:27 +0900

    Compose List sample commit

---
+++ app/src/main/java/com/test/composesample/sampleList/SampleList1.kt
@@ -0,0 +1,44 @@
+package com.test.composesample.sampleState
+
+import android.R
+import android.content.Context
+import android.content.res.Resources
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TabRowDefaults.Divider
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+
+class SampleState1 {
+    @Composable
+    fun HelloContent() {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = "Hello!",
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = MaterialTheme.typography.h5
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = { },
+                label = { Text("Name") }
+            )
+        }
+    }
+
+
+}
--- app/src/main/java/com/test/composesample/sampleState/SampleState4RestoringState.kt
+++ app/src/main/java/com/test/composesample/sampleState/SampleState4RestoringState.kt
@@ -3,6 +3,7 @@ package com.test.composesample.sampleState
 import android.R
 import android.content.Context
 import android.content.res.Resources
+import android.os.Parcelable
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.clickable
@@ -17,37 +18,65 @@ import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.TabRowDefaults.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.mapSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.unit.dp
 import com.test.composesample.TAG
+import kotlinx.parcelize.Parcelize
 
 
-class SampleState3 {
+class SampleState4RestoringState {
+    // Normal - 제일 간단한 방법은 state object를parcelize로 바꿔서 저장하는 방법
+    @Parcelize
+    data class CityOne(val name: String, val country: String) : Parcelable
+
     @Composable
-    fun HelloScreen() {
-        var name by rememberSaveable { mutableStateOf("") }   // Configuration Changed 에서도 값을 유지시켜줍니다. 회전시켜도 값이 유지됩니다.
-//        var name by remember { mutableStateOf("") }   // Configure Changed에서 값을 유지시켜주지않아 회전시키면 사라집니다.
-        HelloContent(name = name, onNameChange = { name = it})
+    fun CityScreen() {
+        var selectedCity = rememberSaveable {
+            mutableStateOf(CityOne("Madrid", "Spain"))
+        }
+    }
+
+
+
+//    MapSaver
+//    Map 형태인 key / value로 object의 속성 값을 직접 지정하여, 저장할 방법을 명시해 줄 수도 있습니다
+    data class CityMapSaver(val name: String, val country: String)
+
+    val CityMapSaveSaver = run {
+        val nameKey = "Name"
+        val countryKey = "Country"
+        mapSaver(
+            save = { mapOf(nameKey to it.name, countryKey to it.country) },
+            restore = { CityMapSaver(it[nameKey] as String, it[countryKey] as String) }
+        )
     }
 
     @Composable
-    private fun HelloContent(name: String, onNameChange: (String) -> Unit) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(
-                text = "Hello, $name",
-                modifier = Modifier.padding(bottom = 8.dp),
-                style = MaterialTheme.typography.h5
-            )
-            OutlinedTextField(
-                value = name,
-                onValueChange = onNameChange,
-                label = {
-                    Text("Name")
-                }
-            )
+    fun CityScreenMapSaver() {
+        var selectedCity = rememberSaveable(stateSaver = CityMapSaveSaver) {
+            mutableStateOf(CityMapSaver("Madrid", "Spain"))
+        }
+    }
+
+
+//    ListSaver
+//    좀 더 간단하게 list 형태로도 StateSaver를 만들 수 있습니다. [1]
+    data class CityListSaver(val name: String, val country: String)
+
+    val CityListSaveSaver = listSaver<CityListSaver, Any>(
+        save = { listOf(it.name, it.country) },
+        restore = { CityListSaver(it[0] as String, it[1] as String) }
+    )
+
+    @Composable
+    fun CityScreenListSaver() {
+        var selectedCity = rememberSaveable(stateSaver = CityListSaveSaver) {
+            mutableStateOf(CityListSaver("Madrid", "Spain"))
         }
     }
 }
